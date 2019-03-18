37ca5fdaabb0869b2e54707e5f2eca7bf33aad5c
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-03-18 11:42:30 -0400

    chore: upgrade 1.1.0
    Change-Id: I04d136c89bcd120517b630c8f2b389e8f9a6b3cd

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_text_area_app",
-    version="1.0.1",
+    version="1.1.0",
     description="Text Area module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
