8db6942290169ca2901fee898d00092d6203c449
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:38 -0400

    fix: revert django upgrade
    Change-Id: I14571815d3d332d83d7c6cf5ec0dda3e4519a5cf

--- core_module_text_area_app/urls.py
+++ core_module_text_area_app/urls.py
@@ -1,10 +1,9 @@
 """ Url router for the Text Area module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_text_area_app.views.views import TextAreaModule
 
 urlpatterns = [
-    re_path(r'module-text-area', TextAreaModule.as_view(), name='core_module_text_area_view'),
+    url(r'module-text-area', TextAreaModule.as_view(), name='core_module_text_area_view'),
 ]
