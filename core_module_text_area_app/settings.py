841aaea54686391803efe4a99213a2bf6849ff34
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:46:42 -0400

    docs: add settings doc
    Change-Id: I1554c755cac29946d4301ff0a0a92d30247808ad

--- core_module_text_area_app/settings.py
+++ core_module_text_area_app/settings.py
@@ -1,4 +1,7 @@
 """ Core module text area settings
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 from django.conf import settings
 
